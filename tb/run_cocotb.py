e0dcab8980af9db5e5c8f049c65c4af61e2cfbc7
NickSica <njs82@drexel.edu>
2021-05-20 19:54:48 -0400

    Changes cocotb test running method

---
+++ tb/run_cocotb.py
@@ -0,0 +1,37 @@
+import configparser
+import os
+import subprocess
+
+import h5py
+
+def setup():
+    config = configparser.ConfigParser()
+    config.read("tb/snn_toolbox.ini")
+
+    h5_file = config["paths"]["filename_ann"] + "_snn" + ".h5"
+    h5 = h5py.File(h5_file, 'r')
+
+    # Takes the first threshold and runs with it
+    v_thresh = h5["model_weights"]["0Dense_2"]["v_thresh:0"].shape
+    param_str = ""
+    if not v_thresh:
+        param_str = " -PTHRESHOLD=0"
+    else:
+        param_str = " -PTHRESHOLD=" + h5["model_weights"]["0Dense_2"]["v_thresh:0"].shape[0]
+
+    if "COMPILE_ARGS" in os.environ:
+        os.environ["COMPILE_ARGS"] = os.environ["COMPILE_ARGS"] + param_str
+    else:
+        os.environ["COMPILE_ARGS"] = param_str
+
+    print(os.getcwd())
+    run_make = "make -f " + os.getcwd() + "/tb/tb.mk"
+    make_proc = subprocess.run(run_make, shell=True, stderr=subprocess.STDOUT)
+    ## Loops through all the h5 values- not needed atm
+    #for key in h5["model_weights"].keys():
+    #    for key, value in dict(h5["model_weights"][key]).items():
+    #        if key == "v_thresh":
+
+
+if __name__ == "__main__":
+    setup()
--- tb/snn_tb.py
+++ tb/snn_tb.py
@@ -3,8 +3,6 @@ from cocotb.triggers import Timer
 from cocotb.triggers import FallingEdge
 from cocotb.clock import Clock
 
-import configparser
-
 from snntoolbox.bin.run import main
 import h5py
 
@@ -12,20 +10,13 @@ import h5py
 async def test(dut):
     dut._log.info("Running test...")
 
-    config = configparser.ConfigParser()
-    config.read("tb/snn_toolbox.ini")
-
     main("tb/snn_toolbox.ini")
-    h5_file = config["paths"]["filename_ann"] + "_INI" + ".h5"
-    h5 = h5py.File(h5_file, 'r')
-
     cocotb.fork(Clock(dut.clk_i, 1, units="ns").start())
     await Timer(0.5, units="ns")
-    dut._log.info(h5.keys())
     dut.rst_i <= 1
-    dut.syn_weights_i <= 0 # TODO: parse h5 and deliver weights
-    dut.ecg_i <= 0
-    dut.delta_i <= 0
+    #dut.syn_weights_i <= 0 # TODO: parse h5 and deliver weights
+    #dut.ecg_i <= 0
+    #dut.delta_i <= 0
     await Timer(1, units="ns")
     dut.rst_i <= 0
 
