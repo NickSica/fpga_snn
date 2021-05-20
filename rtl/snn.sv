d914f6c8ad8a0cb813e04b840b772647a3d1311f
NickSica <njs82@drexel.edu>
2021-05-19 22:33:40 -0400

    Adds basic h5 parsing to testbench

--- rtl/snn.sv
+++ rtl/snn.sv
@@ -1,3 +1,5 @@
+`timescale 1ns/1ps
+
 module snn
 	#(parameter NUM_NODES = 1)
 	(input  logic clk_i,
--- tb/snn_tb.py
+++ tb/snn_tb.py
@@ -5,7 +5,7 @@ from cocotb.clock import Clock
 
 import configparser
 
-import snntoolbox
+from snntoolbox.bin.run import main
 import h5py
 
 @cocotb.test()
@@ -13,19 +13,23 @@ async def test(dut):
     dut._log.info("Running test...")
 
     config = configparser.ConfigParser()
-    config.read("snn_toolbox.ini")
+    config.read("tb/snn_toolbox.ini")
 
-    snntoolbox.bin.run.main("snn_toolbox.ini")
-    h5_file = config["paths"]["path_wd"] + config["paths"]["filename_ann"] + ".h5"
+    main("tb/snn_toolbox.ini")
+    h5_file = config["paths"]["filename_ann"] + "_INI" + ".h5"
     h5 = h5py.File(h5_file, 'r')
 
     cocotb.fork(Clock(dut.clk_i, 1, units="ns").start())
-    await Timer(0.5, unit="ns")
+    await Timer(0.5, units="ns")
+    dut._log.info(h5.keys())
     dut.rst_i <= 1
     dut.syn_weights_i <= 0 # TODO: parse h5 and deliver weights
     dut.ecg_i <= 0
     dut.delta_i <= 0
-    await Timer(1, unit="ns")
+    await Timer(1, units="ns")
     dut.rst_i <= 0
 
     dut.spike_o # TODO: get spike value
+
+    assert True
+    dut._log.info("Running test...done")
