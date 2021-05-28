df3231b14a2963f9e52eb681a59a9ceb5effef37
NickSica <njs82@drexel.edu>
2021-05-27 20:08:58 -0400

    Fleshes out cocotb testbench with accuracy calculation

--- tb/snn_tb.py
+++ tb/snn_tb.py
@@ -6,10 +6,14 @@ from cocotb.triggers import FallingEdge
 from cocotb.triggers import RisingEdge
 from cocotb.clock import Clock
 
+import snntoolbox
+from snntoolbox.simulation.target_simulators.INI_temporal_mean_rate_target_sim import SNN
+from snntoolbox.simulation import utils
 from snntoolbox.bin.run import main
 import pickle
 import h5py
 import numpy as np
+from sklearn import metrics
 
 async def send_inputs(dut, data):
     for idx, i in enumerate(data):
@@ -29,19 +33,39 @@ async def test(dut):
     config = configparser.ConfigParser()
     config.read("tb/snn_toolbox.ini")
 
-    h5_file = config["paths"]["filename_ann"] + ".h5"
-    h5 = h5py.File(h5_file, 'r')
-    print(dict(h5["model_weights"]["dense"]["dense"]))
-    print(dict(h5["model_weights"]["dense_1"]["dense_1"]))
-    print(dict(h5["model_weights"]["lstm"]["lstm"]["lstm_cell"]))
-    print(dict(h5["optimizer_weights"]["RMSprop"]))
-    return
-    weights = []
-    for key in h5["model_weights"].keys():
-        if key != "input_1":
-            weights.append(h5["model_weights"][key]["v_mem:0"].shape[0])
+    #snn = SNN(config)
+    #snn.compile()
+    #dut._log.info(snn.get_weights())
+    #main("tb/snn_toolbox.ini")
+
+    #h5_file = config["paths"]["filename_ann"] + "_weights.h5"
+    weights = [1, 1, 0, -1]
+    #with h5py.File(h5_file, 'r') as h5:
+    #    print(dict(h5))
+    #    print(list(h5["dense"]["dense"]["kernel:0"]))
+    #    print(dict(h5["model_weights"]))
+    #    print(dict(h5["model_weights"]["dense"]))
+    #    print(dict(h5["model_weights"]["dense"]["dense"]))
+    #    print(dict(h5["optimizer_weights"]))
+    #    print(dict(h5["optimizer_weights"]["Nadam"]))
+    #    print(dict(h5["optimizer_weights"]["Nadam"]["dense"]))
+    #    print(dict(h5["model_weights"]["06Dense_10"]))
+    #    print(list(h5["model_weights"]["0Dense_4"]["v_mem:0"]))
+    #    print(dict(h5["model_weights"]["0Dense_4"]["0Dense_4"]))
+
+    #    for key in h5["model_weights"].keys():
+    #        if key != "input_1":
+    #            model_mem = h5["model_weights"][key]["v_mem:0"]
+    #            for i in range(0, model_mem.shape[1]):
+    #                weights.append(int(model_mem[0][i]))
+
     dut._log.info("Weights: " + str(weights))
-    main("tb/snn_toolbox.ini")
+    dut._log.info("Parameters:")
+    dut._log.info("NUM_NODES: " + str(dut.NUM_NODES.value))
+    dut._log.info("CLK_DIV: " + str(dut.CLK_DIV.value))
+    dut._log.info("DEFAULT_UTHR: " + str(dut.DEFAULT_UTHR.value))
+    dut._log.info("DEFAULT_LTHR: " + str(dut.DEFAULT_LTHR.value))
+    dut._log.info("THRESHOLD: " + str(dut.THRESHOLD.value))
 
     dut.rst_i <= 1
     cocotb.fork(Clock(dut.clk_i, 1, units="ns").start())
@@ -59,21 +83,37 @@ async def test(dut):
     data = np.load("data/x_test.npz")
     data = list(np.concatenate(data["arr_0"]).flat)
 
+    dut._log.info("Weights:")
+    for i in range(dut.NUM_NODES.value):
+        dut._log.info(dut.g_neurons[i].N0.synapse_r.value)
+
     input_proc = cocotb.fork(send_inputs(dut, data))
     spike_output = []
     while True:
         await RisingEdge(dut.clk_i)
         time = cocotb.utils.get_sim_time(units="ns")
         #dut._log.info("Time : " + str(time) + " Spike: " + str(dut.spike_o))
-        spike_output.append(str(dut.spike_o.value))
+        spike_output.append(int(dut.spike_o.value))
         if time == len(data) or str(dut.spike_o.value) == 'x':
             break
 
-    print(spike_output)
+    y_pred = np.load("data/y_test.npz")
+    y_pred = list(np.concatenate(y_pred["arr_0"]).flat)
+
+    #with open("tb/output.data", "rb") as data_file:
+    #    spike_output = pickle.load(data_file)
+
     with open("tb/output.data", "wb") as data_file:
         pickle.dump(spike_output, data_file)
 
-    ## TODO: Figure out how to feed snntoolbox
+    print("Spike output length: " + str(len(spike_output)) + " y_pred length: " + str(len(y_pred)))
+    spike_output = spike_output[:len(y_pred)]
+    conf_mat = metrics.confusion_matrix(spike_output, y_pred)
+    conf_mat = np.flip(conf_mat)
+    dut._log.info("Confusion matrix: " + str(conf_mat))
+    acc = (conf_mat[0][0] + conf_mat[-1][-1]) / np.sum(conf_mat)
+    dut._log.info("Accuracy 1: " + str(acc))
+    acc = metrics.accuracy_score(spike_output, y_pred)
+    dut._log.info("Accuracy 2: " + str(acc))
 
     assert True
-    dut._log.info("Running test...done")
